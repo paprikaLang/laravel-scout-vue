1e8b6999e5804c3508d7657096a5eb6c3725bb10
paprikaLang <3060866223@qq.com>
2018-02-06 15:52:02 +0800

    laravel/scout

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('/search',[
+    'as'=> 'api.search',
+    'uses' => 'Api\SearchController@search'
+]);
